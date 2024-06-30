2ca1d16bb864534b414482bae829b71b4af123c7
DEB RAJBANSHI <debrajbanshi1@gmail.com>
2024-06-30 20:22:09 +0530

    complited

---
+++ src/components/useMousePosition.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
+const useMousePosition = () => {
+    const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+    const updateMousePosition = (e: { clientX: any; clientY: any; }) => {
+        setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    useEffect(() => {
+        window.addEventListener("mousemove", updateMousePosition);
+        console.log(mousePosition);
+        
+        return () => window.removeEventListener("mousemove", updateMousePosition);
+    }, []);
+
+    return mousePosition;
+};
+
+export default useMousePosition;
